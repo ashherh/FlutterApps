5e4b395a33e28450b39e7468f9b462a8e080a563
ashherh <ashher.haque@icloud.com>
2019-10-29 11:45:46 -0400

    2 text fields done, need to make a checklist for milestones

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,11 @@ class NewProject extends StatelessWidget {
 
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
+  final TextEditingController projectName = TextEditingController();
+  final TextEditingController projectRequiremt = TextEditingController();
+  final TextEditingController projectDeadline = TextEditingController();
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,6 +75,57 @@ class NewProject extends StatelessWidget {
         child: Container(
           child: Form(
             key: _formkey,
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                const SizedBox(height: 20),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                        width: 1.5,
+                        style: BorderStyle.solid
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15.0)
+                      ),
+                    ),
+                    //prefixIcon: Icon(Icons.aspect_ratio),
+                    labelText: "Project Name",
+                  ),
+                  maxLines: 1,
+                ),
+
+                const SizedBox(height: 20),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                        width: 1.5,
+                        style: BorderStyle.solid
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15.0)
+                      ),
+                    ),
+                    //prefixIcon: Icon(Icons.list),
+                    labelText: "Project Requirement",
+                    alignLabelWithHint: true,
+                    
+                  ),
+                  maxLines: 4,
+                ),
+
+
+              ],
+            ),
+            ),
 
           )
         )
